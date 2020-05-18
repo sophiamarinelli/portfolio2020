eeb614ba22aeef1acc94678a1a648dfff472e0ff
sophiamarinelli <maris170@newschool.edu>
2020-05-18 15:18:24 -0600

    click without random

--- script.js
+++ script.js
@@ -11,67 +11,25 @@ function getRandomColor() {
 
 
 $(document).ready(function() {
-    let gradient = [];
-    for (var i = 0; i < 3; i++) {
-        var value = getRandomColor();
-        gradient.push(value);
-    }
-    $('.caption').css({
-        'background': `${gradient[0]}`,
-        'color': `${gradient[1]}`
-    });
-
-    $('a').css({
-        'color': `${gradient[1]}`
-    });
-
-    $('.header').css({
-        'background': `${gradient[0]}`,
-        'color': `${gradient[1]}`
-    });
-
-    $('body').css({
-        'background': `${gradient[1]}`
-    });
-
-    $('#updateColor').css({
-        'background': `${gradient[1]}`,
-        'color': `${gradient[0]}`
-    });
-
-    $('.footer').css({
-        'color': `${gradient[0]}`
-    });
     let windowWidth = $(window).width()
-    if (windowWidth >= 500) {
-        $('.date').css({
-            'color': `${gradient[0]}`
-        });
-        $('.caption').on('click tap swipe', function() {
-            console.log('trigger')
-            var randomX = Math.floor((Math.random() * 200));
-            var randomY = Math.floor((Math.random() * 200));
-            var sibcount = $(this).siblings().children('.project-image:visible').length;
-            var count = $(this).children('.project-image:hidden').length;
-            console.log(count)
-            var latest = $(this).children('.project-image:hidden:first');
-            var src = $(this).children('.project-image:hidden:first').data('src');
-            if (sibcount >= 1) {
-                $(this).siblings().children('img').hide();
-                latest.attr('src', src).css({ 'z-index': '99', 'top': `${randomX}px`, 'left': `${randomY}px`, }).show();
-            } else if (count >= 1) {
-                latest.attr('src', src).css({ 'z-index': '99', 'top': `${randomX}px`, 'left': `${randomY}px`, }).show();
-            } else {
-                $(this).children('img, video').hide();
-            }
-        });
-    } else {
-        $('.date').css({
-            'color': `${gradient[1]}`
-        });
-        $('.project-image').css({ 'display': 'block', 'position': 'static' })
-
-    }
+    $('.caption').on('click tap swipe', function() {
+        console.log('trigger')
+        var randomX = Math.floor((Math.random() * 200));
+        var randomY = Math.floor((Math.random() * 200));
+        var sibcount = $(this).siblings().children('.project-image:visible').length;
+        var count = $(this).children('.project-image:hidden').length;
+        console.log(count)
+        var latest = $(this).children('.project-image');
+        var src = $(this).children('.project-image:hidden:first').data('src');
+        if (sibcount >= 1) {
+            $(this).siblings().children('img').hide();
+            latest.attr('src', src).css({ 'display': 'block', 'top': `${randomX}px`, 'left': `${randomY}px`, }).show();
+        } else if (count >= 1) {
+            latest.attr('src', src).css({ 'z-index': '99', 'top': `${randomX}px`, 'left': `${randomY}px`, }).show();
+        } else {
+            $(this).children('img, video').hide();
+        }
+    });
 
 
 
