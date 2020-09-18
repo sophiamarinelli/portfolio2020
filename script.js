8430f00d09e437c20b8a8ecd611760756b9d36ee
sophiamarinelli <maris170@newschool.edu>
2020-09-18 13:33:59 -0400

    cross your fingers

--- script.js
+++ script.js
@@ -12,24 +12,24 @@ function getRandomColor() {
 
 $(document).ready(function() {
     let windowWidth = $(window).width()
-    $('.caption').on('click tap swipe', function() {
-        console.log('trigger')
-        var randomX = Math.floor((Math.random() * 200));
-        var randomY = Math.floor((Math.random() * 200));
-        var sibcount = $(this).siblings().children('.project-image:visible').length;
-        var count = $(this).children('.project-image:hidden').length;
-        console.log(count)
-        var latest = $(this).children('.project-image');
-        var src = $(this).children('.project-image:hidden:first').data('src');
-        if (sibcount >= 1) {
-            $(this).siblings().children('img').hide();
-            latest.attr('src', src).css({ 'display': 'block', 'top': `${randomX}px`, 'left': `${randomY}px`, }).show();
-        } else if (count >= 1) {
-            latest.attr('src', src).css({ 'z-index': '99', 'top': `${randomX}px`, 'left': `${randomY}px`, }).show();
-        } else {
-            $(this).children('img, video').hide();
-        }
-    });
+    // $('.caption').on('click tap swipe', function() {
+    //     console.log('trigger')
+    //     var randomX = Math.floor((Math.random() * 200));
+    //     var randomY = Math.floor((Math.random() * 200));
+    //     var sibcount = $(this).siblings().children('.project-image:visible').length;
+    //     var count = $(this).children('.project-image:hidden').length;
+    //     console.log(count)
+    //     var latest = $(this).children('.project-image');
+    //     var src = $(this).children('.project-image:hidden:first').data('src');
+    //     if (sibcount >= 1) {
+    //         $(this).siblings().children('img').hide();
+    //         latest.attr('src', src).css({ 'display': 'block', 'top': `${randomX}px`, 'left': `${randomY}px`, }).show();
+    //     } else if (count >= 1) {
+    //         latest.attr('src', src).css({ 'z-index': '99', 'top': `${randomX}px`, 'left': `${randomY}px`, }).show();
+    //     } else {
+    //         $(this).children('img, video').hide();
+    //     }
+    // });
 
 
 
@@ -81,8 +81,7 @@ $(document).ready(function() {
 
     })
 
-
-    $('.close').on('click tap swipe', function() {
-        //create button to close out, have it appear using conditional
-    })
+// $('.close').on('click tap swipe', function() {
+//     //create button to close out, have it appear using conditional
+// })
 });
